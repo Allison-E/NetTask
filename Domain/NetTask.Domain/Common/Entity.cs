a8fc69ebebed9d89dc32dc8b19a0adfad109b73c
Emmanuel Allison <emmallison13@gmail.com>
2023-03-12 17:41:58 +0100

    feat: models added

---
+++ Domain/NetTask.Domain/Common/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace NetTask.Domain.Common;
+public class Entity
+{
+    /// <summary>
+    /// ID.
+    /// </summary>
+    public Guid Id { get; set; }
+}
---
+++ Domain/NetTask.Domain/Enums/ProgramTypes.cs
@@ -0,0 +1,52 @@
+﻿namespace NetTask.Domain.Enums;
+
+/// <summary>
+/// Program types available.
+/// </summary>
+public enum ProgramTypes
+{
+    /// <summary>
+    /// An internship.
+    /// </summary>
+    Internship = 1,
+
+    /// <summary>
+    /// A job.
+    /// </summary>
+    Job,
+
+    /// <summary>
+    /// A training program.
+    /// </summary>
+    Training,
+
+    /// <summary>
+    /// A masterclass.
+    /// </summary>
+    Masterclass,
+
+    /// <summary>
+    /// A webinar.
+    /// </summary>
+    Webinar,
+
+    /// <summary>
+    /// A single course.
+    /// </summary>
+    Course,
+
+    /// <summary>
+    /// A live seminar.
+    /// </summary>
+    LiveSeminar,
+
+    /// <summary>
+    /// A volunteering program.
+    /// </summary>
+    Volunteer,
+
+    /// <summary>
+    /// Other types.
+    /// </summary>
+    Other,
+}
