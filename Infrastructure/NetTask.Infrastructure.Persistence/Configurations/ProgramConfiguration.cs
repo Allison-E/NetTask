3ed784464976039d54d00f9c6b181d273bf4edc3
Emmanuel Allison <emmallison13@gmail.com>
2023-03-12 19:47:27 +0100

    feat: configuration for program added

---
+++ Infrastructure/NetTask.Infrastructure.Persistence/Configurations/ProgramConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NetTask.Infrastructure.Persistence.Configurations;
+internal class ProgramConfiguration: IEntityTypeConfiguration<Program>
+{
+    public void Configure(EntityTypeBuilder<Program> builder)
+    {
+        builder.OwnsOne(
+                p => p.AdditionalInfo,
+                ad =>
+                {
+                    ad.ToJsonProperty("AdditionalInfo");
+                    ad.Property(a => a.StartDate).ToJsonProperty("StartDate");
+                    ad.Property(a => a.ApplicationOpenDate).ToJsonProperty("ApplicationOpenDate");
+                    ad.Property(a => a.ApplicationCloseDate).ToJsonProperty("ApplicationCloseDate");
+                    ad.Property(a => a.Duration).ToJsonProperty("Duration");
+                    ad.Property(a => a.MinimumQualification).ToJsonProperty("MinQualification");
+                    ad.Property(a => a.MaximumApplications).ToJsonProperty("MaxApplications");
+                    ad.OwnsOne(a => a.Location);
+                });
+
+        builder.Property(p => p.ETag)
+            .IsETagConcurrency();
+    }
+}
--- Infrastructure/NetTask.Infrastructure.Persistence/Usings.cs
+++ Infrastructure/NetTask.Infrastructure.Persistence/Usings.cs
@@ -1,4 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Logging;
 global using NetTask.Application.Interfaces.Repositories;
+global using NetTask.Domain.Models;
 global using NetTask.Infrastructure.Persistence.Contexts;
