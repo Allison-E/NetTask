431e046b302400e95344090bee2d828e2a977047
Emmanuel Allison <emmallison13@gmail.com>
2023-03-12 22:49:13 +0100

    feat: skills search

---
+++ NetTask/Controllers/v1_0/SkillsController.cs
@@ -0,0 +1,24 @@
+﻿using NetTask.Application.Features.Program.Query;
+
+namespace NetTask.Controllers.v1_0;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class SkillsController: ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SkillsController(IMediator mediator) => _mediator = mediator;
+
+    /// <summary>
+    /// Get skills that match the query
+    /// </summary>
+    /// <param name="query">Search query</param>
+    [HttpGet]
+    [ProducesResponseType(typeof(List<string>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> FindAsync([FromQuery] string query = "")
+    {
+        return Ok(await _mediator.Send(new GetSkillsQuery(query)));
+    }
+}
