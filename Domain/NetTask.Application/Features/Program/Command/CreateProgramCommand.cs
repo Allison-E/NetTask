bd9f436c8c9d00deacd4bae03aaf73e7127e6e69
Emmanuel Allison <emmallison13@gmail.com>
2023-03-13 00:29:01 +0100

    feat: create default application form on program creation

--- Domain/NetTask.Application/Features/Program/Command/CreateProgramCommand.cs
+++ Domain/NetTask.Application/Features/Program/Command/CreateProgramCommand.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using NetTask.Application.Dtos.Program;
 using NetTask.Application.Interfaces.Repositories;
+using Newtonsoft.Json.Linq;
 
 namespace NetTask.Application.Features.Program.Command;
 
@@ -12,12 +13,18 @@ public record CreateProgramCommand(CreateProgramDto Program): IRequest<ReadProgr
 
 internal class CreateProgramCommandHandler: IRequestHandler<CreateProgramCommand, ReadProgramDto>
 {
+    private readonly IApplicationFormRepository _applicationFormRepository;
     private readonly ILogger<CreateProgramCommandHandler> _logger;
     private readonly IMapper _mapper;
     private readonly IProgramRepository _programRepository;
 
-    public CreateProgramCommandHandler(ILogger<CreateProgramCommandHandler> logger, IMapper mapper, IProgramRepository programRepository)
+    public CreateProgramCommandHandler(
+        IApplicationFormRepository applicationFormRepository,
+        ILogger<CreateProgramCommandHandler> logger,
+        IMapper mapper,
+        IProgramRepository programRepository)
     {
+        _applicationFormRepository = applicationFormRepository;
         _logger = logger;
         _mapper = mapper;
         _programRepository = programRepository;
@@ -25,12 +32,62 @@ internal class CreateProgramCommandHandler: IRequestHandler<CreateProgramCommand
 
     public async Task<ReadProgramDto> Handle(CreateProgramCommand request, CancellationToken cancellationToken)
     {
+        var program = await CreateProgramAsync(request.Program);
+
+        await CreateApplicationFormAsync(program.Id);
+
+        var readProgramDto = _mapper.Map<ReadProgramDto>(program);
+        return readProgramDto;
+    }
+
+    private async Task<Domain.Models.Program> CreateProgramAsync(CreateProgramDto program)
+    {
         _logger.LogInformation("Creating program");
 
-        Domain.Models.Program programModel = _mapper.Map<Domain.Models.Program>(request.Program);
+        Domain.Models.Program programModel = _mapper.Map<Domain.Models.Program>(program);
         await _programRepository.AddAsync(programModel);
 
-        var readProgramDto = _mapper.Map<ReadProgramDto>(programModel);
-        return readProgramDto;
+        return programModel;
+    }
+
+    public async Task<Guid> CreateApplicationFormAsync(Guid programId)
+    {
+        var form = GetDefaultApplicationForm(programId);
+        await _applicationFormRepository.AddAsync(form);
+
+        return form.Id;
+    }
+
+    private Domain.Models.ApplicationForm GetDefaultApplicationForm(Guid programId)
+    {
+        return new Domain.Models.ApplicationForm()
+        {
+            CoverPhotoId = null,
+            ProgramId = programId,
+            PersonalInfo = new List<ApplicationQuestion>
+            {
+                new() { Message = "First Name", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
+                new() { Message = "Last Name", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
+                new() { Message = "Email", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
+                new() { Message = "Nationality", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = true, IsMandatory = false, IsInternal = false},
+                new() { Message = "Current Residence", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = true, IsMandatory = false, IsInternal = false},
+                new() { Message = "ID Number", Type = ApplicationQuestionTypes.Number, IsHidden = true, IsMandatory = false, IsInternal = false},
+                new() { Message = "Date of Birth", Type = ApplicationQuestionTypes.Date, IsHidden = true, IsMandatory = false, IsInternal = false},
+                new() { Message = "Gender", Type = ApplicationQuestionTypes.DropDown, IsHidden = true, IsMandatory = false, IsInternal = false, AdditionalInfo = JObject.FromObject(
+                    new DropDownQuestionInfo()
+                    {
+                        Options = new List<string> { "Male", "Female", },
+                        IsOtherEnabled = true,
+                    }),
+                },
+            },
+            Profile = new List<ApplicationQuestion>
+            {
+                new() { Message = "Education", Type = ApplicationQuestionTypes.Paragraph, IsHidden = true, IsInternal = false, IsMandatory = false },
+                new() { Message = "Experience", Type = ApplicationQuestionTypes.Paragraph, IsHidden = false, IsInternal = false, IsMandatory = true },
+                new() { Message = "Resume", Type = ApplicationQuestionTypes.FileUpload, IsHidden = true, IsInternal = false, IsMandatory = false },
+            },
+            AdditionalQuestions = null,
+        };
     }
 }
