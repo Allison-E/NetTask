a227d78395a993fd8370cf23b741377d6a1985ad
Emmanuel Allison <emmallison13@gmail.com>
2023-03-12 23:53:15 +0100

    add: automapper for application form DTOs

---
+++ Domain/NetTask.Application/Mappings/Actions/ApplicationFormMappingActions.cs
@@ -0,0 +1,41 @@
+﻿using NetTask.Application.Dtos.ApplicationForm.Questions;
+
+namespace NetTask.Application.Mappings.Actions;
+public class ApplicationQuestion_To_ReadApplicationFormPersonalInformationQuestion_MappingAction: IMappingAction<ApplicationQuestion, ReadApplicationFormPersonalInformationQuestionDto>
+{
+    public void Process(ApplicationQuestion source, ReadApplicationFormPersonalInformationQuestionDto destination, ResolutionContext context)
+    {
+        if (source.AdditionalInfo is not null)
+        {
+            destination.AdditionalInfo = source.AdditionalInfo.ToObject<dynamic>()!;
+        }
+
+        destination.TypeId = source.Type;
+    }
+}
+
+public class ApplicationQuestion_To_ReadApplicationFormAdditionalQuestion_MappingAction: IMappingAction<ApplicationQuestion, ReadApplicationFormAdditionalQuestionDto>
+{
+    public void Process(ApplicationQuestion source, ReadApplicationFormAdditionalQuestionDto destination, ResolutionContext context)
+    {
+        if (source.AdditionalInfo is not null)
+        {
+            destination.AdditionalInfo = source.AdditionalInfo.ToObject<dynamic>()!;
+        }
+
+        destination.TypeId = source.Type;
+    }
+}
+
+public class ApplicationQuestion_To_ReadApplicationFormProfileQuestion_MappingAction: IMappingAction<ApplicationQuestion, ReadApplicationFormProfileQuestionDto>
+{
+    public void Process(ApplicationQuestion source, ReadApplicationFormProfileQuestionDto destination, ResolutionContext context)
+    {
+        if (source.AdditionalInfo is not null)
+        {
+            destination.AdditionalInfo = source.AdditionalInfo.ToObject<dynamic>()!;
+        }
+
+        destination.TypeId = source.Type;
+    }
+}
---
+++ Domain/NetTask.Application/Mappings/ApplicationFormProfile.cs
@@ -0,0 +1,33 @@
+﻿using NetTask.Application.Dtos.ApplicationForm;
+using NetTask.Application.Dtos.ApplicationForm.Questions;
+using NetTask.Application.Mappings.Actions;
+
+namespace NetTask.Application.Mappings;
+internal class ApplicationFormProfile: Profile
+{
+    public ApplicationFormProfile()
+    {
+        CreateMap<ApplicationForm, ReadApplicationFormDto>();
+        CreateMap<UpdateApplicationFormDto, ApplicationForm>();
+
+        // Read application questions DTOs.
+        CreateMap<ApplicationQuestion, ReadApplicationFormAdditionalQuestionDto>()
+            .BeforeMap<ApplicationQuestion_To_ReadApplicationFormAdditionalQuestion_MappingAction>();
+
+        CreateMap<ApplicationQuestion, ReadApplicationFormPersonalInformationQuestionDto>()
+            .BeforeMap<ApplicationQuestion_To_ReadApplicationFormPersonalInformationQuestion_MappingAction>();
+
+        CreateMap<ApplicationQuestion, ReadApplicationFormProfileQuestionDto>()
+            .BeforeMap<ApplicationQuestion_To_ReadApplicationFormProfileQuestion_MappingAction>();
+
+        // Update application questions DTOs.
+        CreateMap<UpdateApplicationFormAdditionalQuestionDto, ApplicationQuestion>()
+            .ForMember(u => u.AdditionalInfo, opt => opt.Ignore());
+
+        CreateMap<UpdateApplicationFormPersonalInformationQuestionDto, ApplicationQuestion>()
+            .ForMember(u => u.AdditionalInfo, opt => opt.Ignore());
+
+        CreateMap<UpdateApplicationFormProfileQuestionDto, ApplicationQuestion>()
+            .ForMember(u => u.AdditionalInfo, opt => opt.Ignore());
+    }
+}
