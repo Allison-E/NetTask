c9c54859398cd99f5fbec3d1d074260e3d4beaf3
Emmanuel Allison <emmallison13@gmail.com>
2023-03-12 16:09:22 +0100

    feat: web app entry

--- NetTask/Program.cs
+++ NetTask/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var builder = WebApplication.CreateBuilder(args);
+
+#region Configure Services
+builder.Services.AddCors(options => options.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddMemoryCache();
+builder.Services.AddHealthChecks();
+#endregion
+
+#region Configure Pipeline
+var app = builder.Build();
+
+app.UseCors("MyPolicy");
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHealthChecks("/health");
+app.MapControllers();
+app.Run();
+#endregion
