76164f45d775f1517fe772e0fed1670794ca78aa
Emmanuel Allison <emmallison13@gmail.com>
2023-03-12 17:05:09 +0100

    feat: setup web api
    * Swagger added

---
+++ NetTask/Extensions/ServiceExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+internal static class ServiceExtensions
+{
+    internal static void AddSwaggerExtension(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.IncludeXmlComments(string.Format(@$"{AppDomain.CurrentDomain.BaseDirectory}\NetTask.xml"));
+            c.IncludeXmlComments(string.Format(@$"{AppDomain.CurrentDomain.BaseDirectory}\NetTask.Application.xml"));
+            c.IncludeXmlComments(string.Format(@$"{AppDomain.CurrentDomain.BaseDirectory}\NetTask.Domain.xml"));
+
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "NetTask API",
+                Description = "This API will be responsible for operation handling.",
+                Contact = new OpenApiContact
+                {
+                    Name = "Emmanuel Allison",
+                    Email = "emmallison13@gmail.com",
+                }
+            });
+        });
+    }
+
+    internal static void AddApiVersioningExtension(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ApiVersionReader = new UrlSegmentApiVersionReader();
+        });
+
+        services.AddVersionedApiExplorer(options =>
+        {
+            // Add the versioned api explorer, which also adds IApiVersionDescriptionProvider service.
+            // Note: the specified format code will format the version as "'v'major[.minor][-status]".
+            options.GroupNameFormat = "'v'VVV";
+
+            // Note: this option is only necessary when versioning by url segment. The SubstitutionFormat
+            // can also be used to control the format of the WebApi version in route templates.
+            options.SubstituteApiVersionInUrl = true;
+        });
+    }
+}
--- NetTask/Program.cs
+++ NetTask/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddCors(options => options.AddPolicy("MyPolicy", builder =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddMemoryCache();
+builder.Services.AddApiVersioningExtension();
+builder.Services.AddSwaggerExtension();
 builder.Services.AddHealthChecks();
 builder.Services.AddApplicationLayer();
 builder.Services.AddPersistenceLayer(builder.Configuration);
@@ -18,10 +20,11 @@ builder.Services.AddPersistenceLayer(builder.Configuration);
 #region Configure Pipeline
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors("MyPolicy");
 app.UseHttpsRedirection();
 app.UseAuthentication();
-app.UseAuthorization();
 app.UseHealthChecks("/health");
 app.MapControllers();
 app.Run();
