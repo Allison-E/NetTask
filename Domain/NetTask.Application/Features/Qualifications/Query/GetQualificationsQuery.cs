cd9dc432e89b22985e0816e18f743a5a64b35ecd
Emmanuel Allison <emmallison13@gmail.com>
2023-03-13 14:25:51 +0100

    feat: get program types and qualifications

---
+++ Domain/NetTask.Application/Features/Qualifications/Query/GetQualificationsQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using NetTask.Application.Dtos.Enum;
+
+namespace NetTask.Application.Features.Qualifications.Query;
+
+/// <summary>
+/// Query to get program types.
+/// </summary>
+public record GetQualificationsQuery: IRequest<List<ReadEnumDto>>;
+
+internal class GetQualificationsQueryHandler: IRequestHandler<GetQualificationsQuery, List<ReadEnumDto>>
+{
+    private readonly IMapper _mapper;
+
+    public GetQualificationsQueryHandler(IMapper mapper) => _mapper = mapper;
+
+    public Task<List<ReadEnumDto>> Handle(GetQualificationsQuery request, CancellationToken cancellationToken)
+    {
+        return Task.Run(() =>
+        {
+            var enumKeyValues = Util.EnumToDictionary<Domain.Enums.Qualifications>();
+
+            var enumDto = _mapper.Map<List<ReadEnumDto>>(enumKeyValues);
+
+            return enumDto;
+        });
+    }
+}
---
+++ NetTask/Controllers/v1_0/ProgramQualificationsController.cs
@@ -0,0 +1,24 @@
+﻿using NetTask.Application.Dtos.Enum;
+using NetTask.Application.Features.Qualifications.Query;
+
+namespace NetTask.Controllers.v1_0;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/programs/qualifications")]
+[ApiVersion("1.0")]
+public class QualificationsController: ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public QualificationsController(IMediator mediator) => _mediator = mediator;
+
+    /// <summary>
+    /// Get qualifications availiable
+    /// </summary>
+    [HttpGet]
+    [ProducesResponseType(typeof(List<ReadEnumDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _mediator.Send(new GetQualificationsQuery()));
+    }
+}
