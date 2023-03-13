bcadd7dc21401d5d14b8e833aef833729e413a76
Emmanuel Allison <emmallison13@gmail.com>
2023-03-13 01:01:45 +0100

    feat: application form operations

---
+++ NetTask/Controllers/v1_0/ApplicationFormController.cs
@@ -0,0 +1,40 @@
+﻿using NetTask.Application.Dtos.ApplicationForm;
+using NetTask.Application.Features.ApplicationForm.Command;
+using NetTask.Application.Features.ApplicationForm.Query;
+
+namespace NetTask.Controllers.v1_0;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/programs/{programId}/application-form")]
+[ApiVersion("1.0")]
+public class ApplicationFormController: ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ApplicationFormController(IMediator mediator) => _mediator = mediator;
+
+    /// <summary>
+    /// Get application form for program
+    /// </summary>
+    /// <param name="programId">Program ID</param>
+    [HttpGet]
+    [ProducesResponseType(typeof(ReadApplicationFormDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetForProgramAsync(string programId)
+    {
+        return Ok(await _mediator.Send(new GetApplicationFormForProgramQuery(programId)));
+    }
+
+    /// <summary>
+    /// Update application form for program
+    /// </summary>
+    /// <param name="programId">Program ID</param>
+    /// <param name="applicationForm">Application form</param>
+    [HttpPut]
+    [ProducesResponseType(typeof(ReadApplicationFormDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateAsync(string programId, [FromBody] UpdateApplicationFormDto applicationForm)
+    {
+        return Ok(await _mediator.Send(new UpdateApplicationFormCommand(programId, applicationForm)));
+    }
+}
