5097cbc59c775199d73ba48e9774b29a4e4ff752
Emmanuel Allison <emmallison13@gmail.com>
2023-03-13 01:44:36 +0100

    add: workflow models

---
+++ Domain/NetTask.Domain/Models/WorkflowStageVideoQuestionInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace NetTask.Domain.Models;
+
+/// <summary>
+/// Additional information for a question that requires a video.
+/// </summary>
+public class WorkflowStageVideoQuestionInfo
+{
+    /// <summary>
+    /// Video segments.
+    /// </summary>
+    public IList<WorkflowStageVideoSegment> Segments { get; set; }
+}
---
+++ Domain/NetTask.Domain/Models/WorkflowStageVideoSegment.cs
@@ -0,0 +1,28 @@
+﻿namespace NetTask.Domain.Models;
+
+/// <summary>
+/// 
+/// </summary>
+public class WorkflowStageVideoSegment: IHasMaxDuration
+{
+    /// <summary>
+    /// Question for this video segment.
+    /// </summary>
+    public string Question { get; set; }
+
+    /// <summary>
+    /// Additional descripton of what is required foor this video segment.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <inheritdoc/>
+    public int MaxDuration { get; set; }
+
+    /// <inheritdoc/>
+    public VideoTimeUnits Unit { get; set; }
+
+    /// <summary>
+    /// Deadline for video submission.
+    /// </summary>
+    public int DeadlineInDays { get; set; }
+}
