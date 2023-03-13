8231bbde4a9a9338e1e4642954ff015800f6f0e6
Emmanuel Allison <emmallison13@gmail.com>
2023-03-13 14:39:58 +0100

    feat: get application form question types and workflow stage types

---
+++ Domain/NetTask.Application/Features/WorkflowStageTypes/Query/GetWorkflowStageTypesQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using NetTask.Application.Dtos.Enum;
+
+namespace NetTask.Application.Features.WorkflowStageTypes.Query;
+
+/// <summary>
+/// Query to get the types of workflow stages available.
+/// </summary>
+public record GetWorkflowStageTypesQuery: IRequest<List<ReadEnumDto>>;
+
+internal class GetWorkflowStageTypesQueryHandler: IRequestHandler<GetWorkflowStageTypesQuery, List<ReadEnumDto>>
+{
+    private readonly IMapper _mapper;
+
+    public GetWorkflowStageTypesQueryHandler(IMapper mapper) => _mapper = mapper;
+
+    public Task<List<ReadEnumDto>> Handle(GetWorkflowStageTypesQuery request, CancellationToken cancellationToken)
+    {
+        return Task.Run(() =>
+        {
+            var enumKeyValues = Util.EnumToDictionary<Domain.Enums.WorkflowStageTypes>();
+
+            var enumDto = _mapper.Map<List<ReadEnumDto>>(enumKeyValues);
+
+            return enumDto;
+        });
+    }
+}
