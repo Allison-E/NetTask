3a2d09700319c9ff3edbe089ed3e5399219088bd
Emmanuel Allison <emmallison13@gmail.com>
2023-03-13 16:07:38 +0100

    fix: not starting

--- Infrastructure/NetTask.Infrastructure.Persistence/Contexts/ApplicationDbContext.cs
+++ Infrastructure/NetTask.Infrastructure.Persistence/Contexts/ApplicationDbContext.cs
@@ -15,95 +15,93 @@ public class ApplicationDbContext: DbContext
 
         var programID = Guid.NewGuid();
 
-        if (Programs.Count() == 0)
-            modelBuilder.Entity<Program>()
-                .HasData(new List<Program>
-                {
-                    new() {
-                        Title = "Learn C#",
-                        Description = "Learn C# in 20 days.",
-                        Summary = null,
-                        Benefits = null,
-                        Criteria = null,
-                        Created = DateTime.Now,
-                        ETag = Guid.NewGuid().ToString(),
-                        KeySkills = new List<string> { "Programming", "Coding", },
-                        Id = programID,
-                        Modified = DateTime.Now,
-                        AdditionalInfo = new()
+        modelBuilder.Entity<Program>()
+            .HasData(new List<Program>
+            {
+                new() {
+                    Title = "Learn C#",
+                    Description = "Learn C# in 20 days.",
+                    Summary = null,
+                    Benefits = null,
+                    Criteria = null,
+                    Created = DateTime.Now,
+                    ETag = Guid.NewGuid().ToString(),
+                    KeySkills = new List<string> { "Programming", "Coding", },
+                    Id = programID,
+                    Modified = DateTime.Now,
+                    AdditionalInfo = new()
+                    {
+                        Duration = null,
+                        ApplicationCloseDate = DateTime.Now.AddDays(8),
+                        ApplicationOpenDate = DateTime.Now.AddDays(1),
+                        StartDate = DateTime.Now.AddDays(9),
+                        MaxApplications = 300,
+                        Location = new()
                         {
-                            Duration = null,
-                            ApplicationCloseDate = DateTime.Now.AddDays(8),
-                            ApplicationOpenDate = DateTime.Now.AddDays(1),
-                            StartDate = DateTime.Now.AddDays(9),
-                            MaxApplications = 300,
-                            Location = new()
-                            {
-                                Address = "London, UK",
-                               IsRemote = true,
-                            },
-                            Type = Domain.Enums.ProgramTypes.Course,
-                            MinQualification = Domain.Enums.Qualifications.HighSchool,
-                        }
+                            Address = "London, UK",
+                            IsRemote = true,
+                        },
+                        Type = Domain.Enums.ProgramTypes.Course,
+                        MinQualification = Domain.Enums.Qualifications.HighSchool,
                     }
-                });
-
-        if (ApplicationForms.Count() == 0)
-            modelBuilder.Entity<ApplicationForm>()
-            .HasData(new List<ApplicationForm>
-            {
-                new()
-                {
-                    CoverPhotoId = null,
-                    ProgramId = programID,
-                    PersonalInfo = new List<ApplicationQuestion>
-                    {
-                        new() { Message = "First Name", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
-                        new() { Message = "Last Name", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
-                        new() { Message = "Email", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
-                        new() { Message = "Nationality", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = true, IsMandatory = false, IsInternal = false},
-                        new() { Message = "Current Residence", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = true, IsMandatory = false, IsInternal = false},
-                        new() { Message = "ID Number", Type = ApplicationQuestionTypes.Number, IsHidden = true, IsMandatory = false, IsInternal = false},
-                        new() { Message = "Date of Birth", Type = ApplicationQuestionTypes.Date, IsHidden = true, IsMandatory = false, IsInternal = false},
-                        new() { Message = "Gender", Type = ApplicationQuestionTypes.DropDown, IsHidden = true, IsMandatory = false, IsInternal = false, AdditionalInfo = JObject.FromObject(
-                            new DropDownQuestionInfo()
-                            {
-                                Options = new List<string> { "Male", "Female", },
-                                IsOtherEnabled = true,
-                            }),
-                        },
-                    },
-                    Profile = new List<ApplicationQuestion>
-                    {
-                        new() { Message = "Education", Type = ApplicationQuestionTypes.Paragraph, IsHidden = true, IsInternal = false, IsMandatory = false },
-                        new() { Message = "Experience", Type = ApplicationQuestionTypes.Paragraph, IsHidden = false, IsInternal = false, IsMandatory = true, AdditionalInfo = JObject.FromObject(
-                            new VideoQuestionInfo()
-                            {
-                                Segments = new List<VideoQuestionSegment>
-                                {
-                                    new() { Description = "Tell us about your last job experience", MaxDuration = 5, Question = "Tell us about experience", Unit  = VideoTimeUnits.Minute },
-                                }
-                            }),
-                        },
-                        new() { Message = "Resume", Type = ApplicationQuestionTypes.FileUpload, IsHidden = true, IsInternal = false, IsMandatory = false },
-                    },
-                    AdditionalQuestions = null,
-                },
+                }
             });
 
-        if (Workflows.Count() == 0)
-            modelBuilder.Entity<Workflow>()
-            .HasData(new List<Workflow>
+        modelBuilder.Entity<ApplicationForm>()
+        .HasData(new List<ApplicationForm>
+        {
+            new()
             {
-                new Workflow()
+                Id = Guid.NewGuid(),
+                CoverPhotoId = null,
+                ProgramId = programID,
+                PersonalInfo = new List<ApplicationQuestion>
                 {
-                    ProgramId = programID,
-                    Stages = new List<WorkflowStage>
-                    {
-                        new() { Name = "Applied", IsVisibleToCandidates = true, Type = WorkflowStageTypes.Shortlisting },
+                    new() { Message = "First Name", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
+                    new() { Message = "Last Name", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
+                    new() { Message = "Email", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = false, IsMandatory = true },
+                    new() { Message = "Nationality", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = true, IsMandatory = false, IsInternal = false},
+                    new() { Message = "Current Residence", Type = ApplicationQuestionTypes.ShortAnswer, IsHidden = true, IsMandatory = false, IsInternal = false},
+                    new() { Message = "ID Number", Type = ApplicationQuestionTypes.Number, IsHidden = true, IsMandatory = false, IsInternal = false},
+                    new() { Message = "Date of Birth", Type = ApplicationQuestionTypes.Date, IsHidden = true, IsMandatory = false, IsInternal = false},
+                    new() { Message = "Gender", Type = ApplicationQuestionTypes.DropDown, IsHidden = true, IsMandatory = false, IsInternal = false, AdditionalInfo = JObject.FromObject(
+                        new DropDownQuestionInfo()
+                        {
+                            Options = new List<string> { "Male", "Female", },
+                            IsOtherEnabled = true,
+                        }),
                     },
                 },
-            });
+                Profile = new List<ApplicationQuestion>
+                {
+                    new() { Message = "Education", Type = ApplicationQuestionTypes.Paragraph, IsHidden = true, IsInternal = false, IsMandatory = false },
+                    new() { Message = "Experience", Type = ApplicationQuestionTypes.Paragraph, IsHidden = false, IsInternal = false, IsMandatory = true, AdditionalInfo = JObject.FromObject(
+                        new VideoQuestionInfo()
+                        {
+                            Segments = new List<VideoQuestionSegment>
+                            {
+                                new() { Description = "Tell us about your last job experience", MaxDuration = 5, Question = "Tell us about experience", Unit  = VideoTimeUnits.Minute },
+                            }
+                        }),
+                    },
+                    new() { Message = "Resume", Type = ApplicationQuestionTypes.FileUpload, IsHidden = true, IsInternal = false, IsMandatory = false },
+                },
+                AdditionalQuestions = null,
+            },
+        });
+
+        modelBuilder.Entity<Workflow>()
+        .HasData(new List<Workflow>
+        {
+            new Workflow()
+            {
+                ProgramId = programID,
+                Stages = new List<WorkflowStage>
+                {
+                    new() { Id = Guid.NewGuid(), Name = "Applied", IsVisibleToCandidates = true, Type = WorkflowStageTypes.Shortlisting },
+                },
+            },
+        });
     }
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
